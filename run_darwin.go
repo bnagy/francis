924598d9b1c655ac07f38e892a48c0be0d4f72b1
ben <git@ben.iagu.net>
2016-04-22 09:50:48 +0930

    clarify API in comments

--- run_darwin.go
+++ run_darwin.go
@@ -11,6 +11,9 @@ import (
 	"strings"
 )
 
+// Run runs a command under francis. This API matches the interface for
+// Debugger for some of my other tools, but the `filename` and `memlimit`
+// params are not presently used. The caller should set them to "" and 0
 func (e *Engine) Run(command []string, filename string, memlimit, timeout int) (crash.Info, error) {
 
 	pkg, err := build.Import("github.com/bnagy/francis", ".", build.FindOnly)
@@ -20,6 +23,7 @@ func (e *Engine) Run(command []string, filename string, memlimit, timeout int) (
 	tool := filepath.Join(pkg.Dir, "exploitaben/exploitaben.py")
 
 	// Construct the command array
+	// XXX: This doesn't do anything, I think I forgot to export an env var or something
 	cmdSlice := []string{tool, "-e", "MallocScribble=1", "-e", "MallocGuardEdges=1"}
 	if e.Timeout > 0 {
 		cmdSlice = append(cmdSlice, []string{"-t", strconv.Itoa(e.Timeout)}...)
